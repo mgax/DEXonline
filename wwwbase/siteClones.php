cb904b655cfe38d5dac42a891384b29435d925fa
Constantin Serban-Radoi <costashsrc@gmail.com>
2011-11-07 23:45:29 +0200

    Arranged and commented source file for siteClones.php

--- wwwbase/siteClones.php
+++ wwwbase/siteClones.php
@@ -2,37 +2,30 @@
 
 require_once("../phplib/util.php");
 
+# Select random definition to search.
 $count = db_getArray(db_execute("select count(*) from Definition where status = 0 and length(internalRep) > 250;"));
 
-#echo $count[0];
 $nr = rand(1, $count[0]);
-
-#echo $nr;
 $strnr = (string)$nr;
 
 $type = util_getRequestParameter('t');
 
-#$word = db_getArray(db_execute("select htmlRep from Definition where status = 0 and id = " . $strnr));
-
 $word = db_getArray(db_execute("select htmlRep from Definition where status = 0 and length(internalRep) > 200 limit " . $strnr . ",1;"));
 
+# Parse definition and create string to search
 $v = explode(" ", strip_tags($word[0]));
-#$v = preg_split("\b", strip_tags($word[0]));
 
-$no = rand(1, 7);
-$len = count($v);
 $to_search = "\"";
 
-smarty_assign('page_title', 'New Stuff');
-smarty_assign('Results', $word[0]);
-smarty_assign('Test', "<p></p><p></p>" . $v[$no]);
-
-for($i = 5; $i <= 20; $i++) {
+# Set string to search start + end
+$WORD_START = 5;
+$WORD_NO = 16;
+$WORD_END = $WORD_START + $WORD_NO;
+for($i = $WORD_START; $i <= $WORD_END; $i++) {
 	$to_search .= $v[$i] . " ";
 }
 
 $to_search .= "\"";
-
 $to_search = str_replace(",", " ", $to_search);
 $to_search = str_replace("(", " ", $to_search);
 $to_search = str_replace(")", " ", $to_search);
@@ -48,13 +41,12 @@ $to_search = str_replace("?", " ", $to_search);
 $to_search = str_replace("\\", " ", $to_search);
 $to_search = str_replace("/", " ", $to_search);
 
-smarty_assign('Lungime', "<p></p>" . $len . "<br /> <br /> <br /> " . $to_search);
+$urlGoogle = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0";
+$apiKey = "ABQIAAAAJjSg4ig0a8tq8tf6rkzmRRRkgZ4KowWMOepZjsVjwMpJ1fhnABQTlVg9YVYaGVZuAI6SYGceehM79w";
+$url = $urlGoogle . "&q=". urlencode($to_search) . "&key=" . $apiKey;
 
-$url = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
-    . "q=". urlencode($to_search) . "&key=ABQIAAAAJjSg4ig0a8tq8tf6rkzmRRRkgZ4KowWMOepZjsVjwMpJ1fhnABQTlVg9YVYaGVZuAI6SYGceehM79w";
-
-// sendRequest
-// note how referer is set manually
+# sendRequest
+# note how referer is set manually
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -62,7 +54,7 @@ curl_setopt($ch, CURLOPT_REFERER, "http://www.dexonline.ro");
 $body = curl_exec($ch);
 curl_close($ch);
 
-// now, process the JSON string
+# now, process the JSON string
 $json = json_decode($body);
 
 $rezultate = $json->responseData->results;
@@ -73,10 +65,12 @@ $messageAlert = "";
 $blackList = "";
 
 foreach($rezultate as $iter) {
+	# Skip dexonline.ro from results
 	if(stripos($iter->url, "dexonline.ro") == true)
 		continue;
+	
 	$lista .= $iter ->url ." <br />";
-	#$content .= $iter -> content . "<br /><br />";
+	# Search for "licenta GPL" or "dexonline.ro" in resulted page
 	$content = @file_get_contents($iter->url);
 	
 	$poslink = stripos($content, "dexonline.ro");
@@ -92,13 +86,13 @@ foreach($rezultate as $iter) {
 
 }
 
-#$toks = explode($content, " ");
+# Print Blacklist items if any
 
+smarty_assign('page_title', 'New Stuff');
+smarty_assign('Results', $word[0]);
 
 smarty_assign('JSON', "<p></p><br />" . $lista);
-#smarty_assign('CRAWL_THROUGH', "<p></p><br />" . $content);
 smarty_assign('ALERT', "<p></p><br />" . $messageAlert);
-#smarty_assign('JSON', "<p></p><br />" . $json ->responseData -> results[0] -> url );
 smarty_assign("CRAWL_THROUGH", "<p><b>Blacklist</b></p><br />" . $blackList);
 smarty_displayCommonPageWithSkin("siteClones.ihtml");
 
