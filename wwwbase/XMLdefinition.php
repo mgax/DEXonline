e15a5221c684d2b0f3729c0a58eaaf99807124a8
Vlad Traista-Popescu <vladtp91@gmail.com>
2011-11-21 23:13:28 +0200

    Ticket #69

---
+++ wwwbase/XMLdefinition.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("../phplib/util.php");
+
+if(!isset($_GET['cuvant']))
+	exit(0);
+
+$cuv = mysql_escape_string($_GET['cuvant']);
+
+if ( $cuv == "" )
+	exit(0);
+
+$word = db_getSingleValue ( "select htmlRep from Definition where lexicon = \"$cuv\" ;") ;
+
+echo $word ;
+
+?>
+
