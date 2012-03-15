7e477606c993c818810b5d0a80177b8b91a97b82
Alexandru Ciprian Farcasanu <ciprianfarcasanu@yahoo.com>
2012-03-15 13:46:18 +0200

    Nothing new. Just some cleanup

---
+++ wwwbase/js/spnz.js
@@ -0,0 +1,10 @@
+function letterPressed (letter) {
+    secret = "This is my little secret"
+    alert ("You pressed: " letter);
+}
+
+function writeText (form) {
+    form.inputbox.value = "Have a nice day!"
+    form.out1.value = form.in1.value;
+    form.out2.value = form.in2.value;
+}
